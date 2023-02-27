f7e0830ea59a4b2fb1e420f46dbae13cb8dc5a3e
Siddhant Srivastava <sidsrivastavasks@gmail.com>
2023-02-27 21:46:11 +0530

    added module

--- assignment-src/assignment/settings.py
+++ assignment-src/assignment/settings.py
@@ -124,4 +124,4 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-ALLOWED_HOSTS = ['excel-parser-django.onrender.com']
+ALLOWED_HOSTS = ['my-django-project.onrender.com']
